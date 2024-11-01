c1b9b8a8235c5ca5f163e35438d4e878827113c5
Yasmindel <159081224+Yasmindel@users.noreply.github.com>
2024-11-01 20:15:01 +0100

    Update ps.py

--- ps.py
+++ ps.py
@@ -1,16 +1,16 @@
 def main():
-    # Read the number of elements
-    n = int(input("Enter the number of elements: "))
-    # Read the elements into a list
-    a = list(map(int, input("Enter the elements: ").split()))
-    # Read the number of smallest elements to return
-    k = int(input("Enter the number of smallest elements to display: "))
+    # Number of elements in the list
+    n = 10
+    # Elements in the list
+    a = [4, -6, 7, 8, -9, 100, 12, 13, 56, 17]
+    # Number of smallest elements to return
+    k = 3
     
-    # Sort the list and get the first k elements
+    # Sort the list and get the first k smallest elements
     smallest_elements = sorted(a)[:k]
     
     # Print the result
     print(' '.join(map(str, smallest_elements)))
 
-if name == '__main__':
+if __name__ == '__main__':
     main()
